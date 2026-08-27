f7e4acf10d631c938bbc2117ea26520eaf78d714
DESKTOP-0IEO1SD\DREAMWORLD <prachilakhani.software@gmail.com>
2026-08-27 17:17:09 +0530

    no message

--- studentmanagement/settings.py
+++ studentmanagement/settings.py
@@ -178,9 +178,9 @@ from pathlib import Path
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+     BASE_DIR / "static",
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR /"media"
